76be7e1aac18b309ba8e72b9422ea60be643a1cc
unknown <serdominguez@gmail.com>
2020-05-07 10:01:15 -0300

    subido por consola

---
+++ TP-02 - Alumno/TP-02/Entidades/Moto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Moto : Vehiculo
+    {
+        public Moto(EMarca marca, string codigo, ConsoleColor color)
+            :base(codigo, marca, color)
+        {
+        }
+
+        /// <summary>
+        /// Las motos son chicas
+        /// </summary>
+        public override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Chico;
+            }
+        }
+
+        public override string Mostrar() 
+        {
+            StringBuilder sb = new StringBuilder();
+
+            
+            sb.AppendLine("MOTO");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("TAMAÑO : {0}", this.Tamanio.ToString());
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
